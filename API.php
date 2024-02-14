e3e785a76edad3386184d799f47aa20f6175827f
Benjamin Klemencic <benjamin.klemencic@fau.de>
2024-02-14 09:24:35 +0100

    uniportal_shorten_url_callback() : update if exists, else insert

--- API.php
+++ API.php
@@ -23,17 +23,24 @@ function uniportal_shorten_url_callback($request) {
     // Get the URL to shorten from the request parameters
     $url_to_shorten = $parameters['url'];
 
-    // Shorten the URL using the UniportalShortURL class
-    $shortened_url = UniportalShortURL::shorten($url_to_shorten);
-
-    // Store the original and shortened URLs in the database
+    // Check if the original URL already exists in the database
     global $wpdb;
     $table_name = $wpdb->prefix . 'short_urls';
-    $wpdb->insert($table_name, array(
-        'original_url' => $url_to_shorten,
-        'shortened_url' => $shortened_url,
-        'created_at' => current_time('mysql'),
-    ));
+    $existing_row = $wpdb->get_row($wpdb->prepare("SELECT * FROM $table_name WHERE original_url = %s", $url_to_shorten));
+
+    if ($existing_row) {
+        // If the original URL exists, update the corresponding row
+        $wpdb->update($table_name, array('shortened_url' => $existing_row->shortened_url), array('id' => $existing_row->id));
+        $shortened_url = $existing_row->shortened_url;
+    } else {
+        // If the original URL doesn't exist, shorten it and insert a new row
+        $shortened_url = UniportalShortURL::shorten($url_to_shorten);
+        $wpdb->insert($table_name, array(
+            'original_url' => $url_to_shorten,
+            'shortened_url' => $shortened_url,
+            'created_at' => current_time('mysql'),
+        ));
+    }
 
     // Return the shortened URL in the response
     return rest_ensure_response($shortened_url);
