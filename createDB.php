1f95c9866433c5603b6e4a66bd9ea92af59234ac
Benjamin Klemencic <benjamin.klemencic@fau.de>
2024-02-14 09:27:38 +0100

    createDB : statistic table

--- createDB.php
+++ createDB.php
@@ -2,14 +2,16 @@
 
 namespace RRZE\ShortURL;
 
-// Activation hook to create the database table
-register_activation_hook(__FILE__, 'uniportal_short_url_create_table');
+// Activation hook to create the database tables
+register_activation_hook(__FILE__, 'uniportal_short_url_create_tables');
 
-function uniportal_short_url_create_table() {
+function uniportal_short_url_create_tables() {
     global $wpdb;
-    $table_name = $wpdb->prefix . 'short_urls';
     $charset_collate = $wpdb->get_charset_collate();
-    $sql = "CREATE TABLE $table_name (
+
+    // Create short_urls table
+    $short_urls_table_name = $wpdb->prefix . 'short_urls';
+    $short_urls_sql = "CREATE TABLE $short_urls_table_name (
         id mediumint(9) NOT NULL AUTO_INCREMENT,
         original_url text NOT NULL,
         shortened_url text NOT NULL,
@@ -17,5 +19,17 @@ function uniportal_short_url_create_table() {
         PRIMARY KEY  (id)
     ) $charset_collate;";
     require_once(ABSPATH . 'wp-admin/includes/upgrade.php');
-    dbDelta($sql);
+    dbDelta($short_urls_sql);
+
+    // Create statistics table
+    $statistics_table_name = $wpdb->prefix . 'short_url_statistics';
+    $statistics_sql = "CREATE TABLE $statistics_table_name (
+        id mediumint(9) NOT NULL AUTO_INCREMENT,
+        short_url_id mediumint(9) NOT NULL,
+        iCount int NOT NULL DEFAULT 0,
+        created_at datetime DEFAULT '0000-00-00 00:00:00' NOT NULL,
+        FOREIGN KEY (short_url_id) REFERENCES $short_urls_table_name(id),
+        PRIMARY KEY  (id)
+    ) $charset_collate;";
+    dbDelta($statistics_sql);
 }
