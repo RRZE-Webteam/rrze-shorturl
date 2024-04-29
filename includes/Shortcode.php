0ce8152f6493c28a7b57fcf2c40e1ab184f7c537
Benjamin Klemencic <benjamin.klemencic@fau.de>
2024-04-29 16:22:20 +0200

    closes #23

--- includes/Shortcode.php
+++ includes/Shortcode.php
@@ -624,7 +624,7 @@ class Shortcode
             // Check if URL is provided
             if (!empty($aParams['url'])) {
                 $result = ShortURL::shorten($aParams);
-                $result_message = ($result['error'] ? 'Error: ' : 'Short URL: ') . $result['txt'];
+                $result_message = ($result['error'] ? 'Error: ' : 'Short link: ') . $result['txt'];
                 $result_message .= (!$result['error'] ? '&nbsp;&nbsp;<button type="button" class="btn" id="copyButton" name="copyButton" data-shortened-url="' . $result['txt'] . '"><img class="shorturl-copy-img" src="data:image/svg+xml,%3Csvg height=\'1024\' width=\'896\' xmlns=\'http://www.w3.org/2000/svg\'%3E%3Cpath d=\'M128 768h256v64H128v-64z m320-384H128v64h320v-64z m128 192V448L384 640l192 192V704h320V576H576z m-288-64H128v64h160v-64zM128 704h160v-64H128v64z m576 64h64v128c-1 18-7 33-19 45s-27 18-45 19H64c-35 0-64-29-64-64V192c0-35 29-64 64-64h192C256 57 313 0 384 0s128 57 128 128h192c35 0 64 29 64 64v320h-64V320H64v576h640V768zM128 256h512c0-35-29-64-64-64h-64c-35 0-64-29-64-64s-29-64-64-64-64 29-64 64-29 64-64 64h-64c-35 0-64 29-64 64z\' fill=\'%23FFFFFF\' /%3E%3C/svg%3E" alt="' . __('Copy to clipboard', 'rrze-shorturl') . '"></button>&nbsp;&nbsp;<span id="shorturl-tooltip" class="shorturl-tooltip">' . __('Copied to clipboard', 'rrze-shorturl') . '</span>' : '');
             }
         }
@@ -634,7 +634,7 @@ class Shortcode
         $form .= '<div class="postbox">';
         $form .= '<h2 class="handle">' . __('Create Short URL', 'rrze-shorturl') . '</h2>';
         $form .= '<div class="inside">';
-        $form .= '<label for="url">' . __('Long URL', 'rrze-shorturl') . ':</label>&nbsp;';
+        $form .= '<label for="url">' . __('Your link', 'rrze-shorturl') . ':</label>&nbsp;';
         $form .= '<input type="text" name="url" id="url" value="' . esc_attr($aParams['url']) . '" placeholder="https://">';
         $form .= '<input type="submit" id="generate" name="generate" value="' . __('Shorten', 'rrze-shorturl') . '">';
         $form .= '<input type="hidden" name="link_id" value="' . (!empty($result['link_id']) ? $result['link_id'] : '') . '">';
@@ -656,7 +656,6 @@ class Shortcode
         if (!empty($result) && !$result['error']) {
             $form .= '<input id="shortened_url" name="shortened_url" type="hidden" value="' . $result['txt'] . '">';
             $form .= '<div id="qr-container"><canvas id="qr"></canvas><img src="' . plugins_url('../', __FILE__) . 'assets/img/FAU.svg' . '" id="qr-logo"></div>';
-            // $form .= '<div id="qr-container"><canvas id="qr"></canvas></div>';
         }
         $form .= '</form>';
 
