4407e5deb4d15eddfef39285c3b8855488d480be
Benjamin Klemencic <benjamin.klemencic@fau.de>
2024-05-13 15:45:49 +0200

    1.1.23 (redirect services using endpoints strictly)

--- redirect-services.php
+++ redirect-services.php
@@ -1,34 +1,6 @@
 <?php
 
-// 9wv0h => 1196144
-
-
 $shorturl_domain = "https://go-fau.test.rrze.fau.de"; // Domain on which the plugin rrze-shorturl runs
-function decryptBase37(string $encrypted): int {
-    $ShortURLModChars = "abcdefghijklmnopqrstuvwxyz0123456789-";
-    $len = strlen($ShortURLModChars); // Basis 37
-
-    $decrypted = 0;
-
-    for ($i = 0; $i < strlen($encrypted); $i++) {
-        $char = $encrypted[$i];
-        $index = strpos($ShortURLModChars, $char);
-
-        if ($index === false) {
-            throw new Exception("Invalid character in encrypted string.");
-        }
-
-        // Die Basis-37-Zahl wird in eine Dezimalzahl umgewandelt
-        $decrypted = $decrypted * $len + $index;
-    }
-
-    return $decrypted;
-}
-
-echo "test<br>";
-$encrypted = "ba";
-echo decryptBase37($encrypted); // Ausgabe: 75
-exit;
 
 try {
     $response = file_get_contents($shorturl_domain . "/wp-json/short-url/v1/services");
@@ -41,7 +13,6 @@ try {
         throw new Exception("Failed to decode JSON response.");
     }
 
-
     $code = htmlspecialchars($_GET["code"]);
     $prefix = (int) $code[0];
     $encrypted = substr($code, 1);
@@ -50,10 +21,14 @@ try {
         if ($service["prefix"] == $prefix) {
             $service_link = $service["regex"];
 
-            $decrypted = decryptBase37($encrypted);
-
-            echo '$decrypted = ' . $decrypted;
-            exit;
+            try {
+                $decrypted = file_get_contents($shorturl_domain . '/wp-json/short-url/v1/decrypt?encrypted=' . $encrypted);
+                if ($decrypted === false) {
+                    throw new Exception("Failed to decrypt using from the REST API endpoint.");
+                }
+            } catch (Exception $e) {
+                echo "Error: " . $e->getMessage();
+            }
 
             $redirect_url = preg_replace('/\$\w+/', $decrypted, $service_link);
 
