dca0407c5fa02fe3f0c8c6138cd15636c11cbe0c
Benjamin Klemencic <benjamin.klemencic@fau.de>
2024-05-21 14:35:44 +0200

    1.3.16 (fixes the path to rest-api endpoints)

--- includes/API.php
+++ includes/API.php
@@ -23,7 +23,7 @@ class API {
     }
 
     public function register_rest_endpoints() {
-        register_rest_route('rrze-shorturl/v1', '/shorten', array(
+        register_rest_route('wp/v2/shorturl/', '/shorten', array(
             'methods' => 'POST',
             'callback' => array($this, 'shorten_url_callback'),
             'permission_callback' => function () {
@@ -31,19 +31,28 @@ class API {
             }
         ));
 
-        register_rest_route('rrze-shorturl/v1', '/get-longurl', array(
+
+        register_rest_route('wp/v2/shorturl/', '/get-longurl', array(
             'methods' => 'GET',
             'callback' => array($this, 'get_longurl_callback'),
             // 'permission_callback' => [$this, 'is_ip_allowed']
         ));
 
-        register_rest_route('rrze-shorturl/v1', '/active-shorturls', array(
+        
+
+        register_rest_route('wp/v2/shorturl/', '/get-longurl', array(
+            'methods' => 'GET',
+            'callback' => array($this, 'get_longurl_callback'),
+            // 'permission_callback' => [$this, 'is_ip_allowed']
+        ));
+
+        register_rest_route('wp/v2/shorturl/', '/active-shorturls', array(
             'methods' => 'GET',
             'callback' => array($this, 'get_active_shorturls_callback'),
             // 'permission_callback' => [$this, 'is_ip_allowed']
         ));
 
-        register_rest_route('rrze-shorturl/v1', '/categories', array(
+        register_rest_route('wp/v2/shorturl/', '/categories', array(
             'methods' => 'GET',
             'callback' => array($this, 'get_categories_callback'),
             'permission_callback' => function () {
@@ -51,7 +60,7 @@ class API {
             }
         ));
 
-        register_rest_route('rrze-shorturl/v1', '/add-category', array(
+        register_rest_route('wp/v2/shorturl/', '/add-category', array(
             'methods' => 'POST',
             'callback' => array($this, 'add_category_callback'),
             'permission_callback' => function () {
@@ -59,14 +68,14 @@ class API {
             }
         ));  
         
-        register_rest_route('rrze-shorturl/v1', '/services', array(
+        register_rest_route('wp/v2/shorturl/', '/services', array(
             'methods' => 'GET',
             'callback' => array($this, 'get_services_callback'),
             // 'permission_callback' => [$this, 'is_ip_allowed']
         ));
 
 
-        register_rest_route('rrze-shorturl/v1', '/decrypt', array(
+        register_rest_route('wp/v2/shorturl/', '/decrypt', array(
             'methods' => 'GET',
             'callback' => array($this, 'get_decrypt_callback'),
             // 'permission_callback' => [$this, 'is_ip_allowed']
