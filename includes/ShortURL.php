55c1849b3a0a3776cfc8b075efa0fe13d151007c
Benjamin Klemencic <benjamin.klemencic@fau.de>
2025-11-28 12:15:22 +0100

    debug

--- includes/ShortURL.php
+++ includes/ShortURL.php
@@ -264,8 +264,10 @@ class ShortURL
                 }
             }
 
+            $allowedDomains = static::getAllowedDomains();
+
             // Check if the extracted domain belongs to one of our allowed domains
-            foreach (self::$CONFIG['AllowedDomains'] as $aEntry) {
+            foreach ($allowedDomains as $aEntry) {
                 if ($domain === $aEntry['hostname']) {
                     $notice = $aEntry['notice'];
 
